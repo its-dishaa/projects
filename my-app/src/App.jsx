0a1c5b2a5bc08df5c5ea41fae0dbe174eb7fd52f
Disha <disha.sahay2005@gmail.com>
2025-08-01 19:19:51 +0530

    currencyconverter

---
+++ my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import useCurrencyInfo from './hooks/usecurrencyinfo'
+
+function App() {
+  
+  return (
+    <>
+      <h1>welcome to vite</h1>
+    </>
+  )
+}
+
+export default App
