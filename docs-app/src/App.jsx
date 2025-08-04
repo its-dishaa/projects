8a88f06eea836b435dc93d0f6a1105f5b4f38ce4
Disha <disha.sahay2005@gmail.com>
2025-08-04 16:11:36 +0530

    docs

---
+++ docs-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Background from './components/background';
+
+function App() {
+  return (
+    <div className="container text-center mt-5">
+      <h1 className="display-4 text-primary ">Bootstrap is working 🎉</h1>
+      <button className="btn btn-success mt-3">Click Me</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ docs-app/src/components/Background.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Background(){
+    return(
+        <>
+        <div className="fixed z-[2] w-full h-screen">
+
+        </div>
+        
+        </>
+    )
+}
+
+export default Background;
