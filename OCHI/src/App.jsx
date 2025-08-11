15e80b9c60492d876eaff81ba6e5720c6a274eee
Disha <disha.sahay2005@gmail.com>
2025-08-12 01:15:04 +0530

    updated

--- OCHI/src/App.jsx
+++ OCHI/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import LandingPage from './components/LandingPage';
 import Marquee from './components/Marquee';
+import About from './components/About';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Navbar />
       <LandingPage/>
       <Marquee/>
-
+      <About/>
     </div>
   );
 }
--- OCHI/src/components/marquee.jsx
+++ OCHI/src/components/marquee.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import { motion } from "framer-motion";
+
 
 function Marquee() {
   return (
     <div
-      className="w-100 d-flex flex-column justify-content-center align-items-center bg-success"
-      style={{ height: '50vh' }}
+      className="w-100 d-flex flex-column justify-content-center align-items-center bg-emphasis"
+      style={{ height: '50vh', transform: 'translateY(-130px)'}}
     >
-      <h1 className="display-1 fw-bold text-white">we are ochi</h1>
-      <h1 className="display-1 fw-bold text-white">we are ochi</h1>
+ <div className="border border-white p-3 rounded d-flex gap-3 w-100" 
+ >
+    
+        <motion.div
+          className="d-flex gap-5"
+          initial={{ x: "100%" }}
+          animate={{ x: "-100%" }}
+          transition={{ repeat: Infinity, duration: 5, ease: "linear" }}
+        >
+          <h1 className="display-1 fw-bold text-white mb-0">we are ochi</h1>
+          <h1 className="display-1 fw-bold text-white mb-0">we are ochi</h1>
+        </motion.div>
+    </div>
     </div>
   );
 }
